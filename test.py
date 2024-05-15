0ba7e5ccfad3756b4310d2e1de3840a4495b6c1e
vogaliccb <86399985+vogaliccb@users.noreply.github.com>
2024-05-15 13:38:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 def trigger_exception():
     try:
         # This line will raise a ZeroDivisionError
-        result = 10 / 0
+        result = 10 / 1
         print(result)  # This line won't be executed
     except ZeroDivisionError as e:
         # Handle the exception
